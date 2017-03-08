20cfff7b38b7beb929259c97c9e6b8ea7e862e58
Rich Naylor <rich.naylor@nike.com>
2017-03-08 11:53:55 -0800

    got rid of some old comments

--- app.js
+++ app.js
@@ -1,44 +1,24 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-// var passport = require('passport');
 var morgan = require('morgan');
-// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var moment = require('moment');
-
-// var session = require('express-session');
-// var fs = require('fs');
-
-
-// var config = require('./config/config')[env];
-// require('./config/passport')(passport, config);
 
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// app.use(express.static('public'));
 app.use(express.static(__dirname + '/public'));
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(session(
-//   {
-//     resave: true,
-//     saveUninitialized: true,
-//     secret: 'this shit hits'
-//   }));
-// app.use(passport.initialize());
-// app.use(passport.session());
 app.use(cookieParser());
 
 // routes
-// require('./config/routes')(app, config);
 require('./config/routes')(app);
 
 // catch 404 and forward to error handler
--- config/routes.js
+++ config/routes.js
@@ -32,7 +32,10 @@ module.exports = function (app, config) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('charset', 'utf-8');
+    // res.setHeader('charset', 'utf-8');
+    // res.set({ 'content-type': 'application/json;charset=utf-8' });
+    res.setHeader('Content-Type', 'application/json');
+
     next();
   });
 
