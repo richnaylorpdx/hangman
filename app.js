65fd9aab7a48986be88b388f85b36d00b3f4d526
Rich Naylor <rich.naylor@nike.com>
2017-03-08 06:59:26 -0800

    added explanation of design and game play to readme. modified app.js and removed detail of port running info to remove potential confusion

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(function(err, req, res, next) {
 });
 
 app.listen(3000, function () {
-  console.log('Hangman listening on port ' + 3000);
+  console.log('Hangman is up and running');
 });
 
 module.exports = app;
